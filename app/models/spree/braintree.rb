3509ee956779ee194d8fb52811e5cd7ff38c444e
Jim <jdurand@o2web.ca>
2013-09-11 18:22:31 -0400

    allow mass assign attributes

--- app/models/spree/braintree.rb
+++ app/models/spree/braintree.rb
@@ -2,6 +2,8 @@ class Spree::Braintree < Spree::Gateway
 	preference :merchant_id, :string
 	preference :private_key, :string
 	preference :public_key, :string
+  
+  attr_accessible :preferred_merchant_id, :preferred_private_key, :preferred_public_key
 
   def provider_class 
     ActiveMerchant::Billing::BraintreeGateway
